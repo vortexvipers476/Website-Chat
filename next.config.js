d162c71367927ac49e242e01fd024bd181afdd0d
vortexvipers476 <vortexvipers1003@gmail.com>
2025-09-16 20:11:21 +0700

    Create next.config.js

---
+++ next.config.js
@@ -0,0 +1,11 @@
+// next.config.js
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  images: {
+    domains: ['picsum.photos'],
+  },
+}
+
+module.exports = nextConfig
